46dcd7c7593367c8e2e1f4849c139ab1159becfa
Alexgoon <alex.russkovdx@gmail.com>
2017-02-17 15:45:23 +0300

    Fix

--- Dashboard_BindingToList/Form1.cs
+++ Dashboard_BindingToList/Form1.cs
@@ -5,8 +5,6 @@ namespace Dashboard_BindingToList {
     public partial class Form1 : XtraForm {
         public Form1() {
             InitializeComponent();
-            int t=256;
-            t++;
         }
 
         private void Form1_Load(object sender, System.EventArgs e) {
